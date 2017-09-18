b49ecc14513aa46d8dbb71db014e94727e4e33e4
Uamhan <uamhanl@gmail.com>
2017-09-18 12:38:40 +0100

    added problem 1 hello world in japanese

---
+++ 01-Problem-Sheet-1.go
@@ -0,0 +1,11 @@
+//PROBLEM ONE : hello world in japaneese.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	fmt.Printf("こんにちは世界.\n") // prints hello world in japaneese characters to the screen.
+}
