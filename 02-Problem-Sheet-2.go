cda9425e73e5d5bfa3dbb3f344414885e7739d9f
Uamhan <uamhanl@gmail.com>
2017-09-18 13:07:23 +0100

    added problem 2 current time

---
+++ 02-Problem-Sheet-2.go
@@ -0,0 +1,13 @@
+//PROBLEM TWO : print current time.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	
+	t := time.Now()// sets varible t to the current time.
+	fmt.Printf("It is currently %v.\n",t) // prints varible t
+}
